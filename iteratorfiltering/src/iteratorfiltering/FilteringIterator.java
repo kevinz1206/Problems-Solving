21fc357dbe20abfdb1239d1bb45de6299fbbfea6
Kevin <kevinz@udel.edu>
2018-02-28 16:03:52 -0500

    final version

--- iteratorfiltering/src/iteratorfiltering/FilteringIterator.java
+++ iteratorfiltering/src/iteratorfiltering/FilteringIterator.java
@@ -1,33 +1,28 @@
 package iteratorfiltering;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
-public class FilteringIterator implements Iterator{
-	private Iterator myIterator;
-	private IObjectTest<?> myTest;
-	private Object o;
+public class FilteringIterator<T> implements Iterator<T>{
+	private Iterator<T> myIterator;
+	private IObjectTest<T> myTest;
+	private T t;
 	private boolean b = false;
-	public FilteringIterator(Iterator myIterator, IObjectTest myTest) {
+	public FilteringIterator(Iterator<T> myIterator, IObjectTest<T> myTest) {
 		super();
 		this.myIterator = myIterator;
 		this.myTest = myTest;
 	}
-	//{1,0,1,0}
 	@Override
 	public boolean hasNext() {
 		if(b) {
 			return true;
 		}
-
 		if(!myIterator.hasNext()) {
 			return false;
 		}else {
-			Object cur = myIterator.next();
+			T cur = myIterator.next();
 			if(myTest.test(cur)) {
-				o = cur;
+				t = cur;
 				b = true;
 				return b;
 			}else {
@@ -37,36 +32,11 @@ public class FilteringIterator implements Iterator{
 	}
 
 	@Override
-	public Object next() {
+	public T next() {
 		if(!hasNext()) {
 			return null;
 		}
 		b = false;
-		return o;
-	}
-	public static void main(String[] args) {
-		List<Integer> li = Arrays.asList(1,2,3);
-		Iterator<Integer> it = li.iterator();
-		FilteringIterator f = new FilteringIterator(it, new IObjectTest<Integer>() {
-			@Override
-			public boolean test(Object o) {
-				
-				if((Integer)o%2 != 0) {
-					return true;
-				}else {
-					return false;
-				}
-			}
-			
-		});
-		System.out.println("a");	
-		System.out.println(f.hasNext());
-
-		System.out.println(f.next());
-		System.out.println(f.next());
-		System.out.println(f.next());
-		System.out.println(f.hasNext());
-
-	}
-	
+		return (T)t;
+	}	
 }
--- iteratorfiltering/src/test/FilteringIteratorTest.java
+++ iteratorfiltering/src/test/FilteringIteratorTest.java
@@ -2,20 +2,41 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import iteratorfiltering.FilteringIterator;
+import iteratorfiltering.IObjectTest;
 
 public class FilteringIteratorTest {
-	FilteringIterator filteringIterator;
+	FilteringIterator<Integer> f;
 	@Before
 	public void init() {
-		
+		List<Integer> li = Arrays.asList(1,2,3);
+		Iterator<Integer> it = li.iterator();
+		f = new FilteringIterator<Integer>(it, new IObjectTest<Integer>() {
+			@Override
+			public boolean test(Object o) {
+				
+				if((Integer)o%2 != 0) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+			
+		});
+
 	}
 	@Test
 	public void test() {
-		
+		assertTrue(f.next() == 1);
+		assertTrue(f.next() == 3);
+		assertEquals(null, f.next());
 	}
 
 }
